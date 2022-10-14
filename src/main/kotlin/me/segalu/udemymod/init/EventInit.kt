d03d1b0498831b330ee453eb2906b3f7976f2224
Segalu <SebaGLuna@outlook.com>
2022-10-14 09:44:30 -0500

    Lesson: Events & Parchment Update

--- src/main/kotlin/me/segalu/udemymod/init/EventInit.kt
+++ src/main/kotlin/me/segalu/udemymod/init/EventInit.kt
@@ -3,7 +3,10 @@ package me.segalu.udemymod.init
 import me.segalu.udemymod.UdemyMod
 import me.segalu.udemymod.command.ReturnHomeCommand
 import me.segalu.udemymod.command.SetHomeCommand
+import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.Items
 import net.minecraftforge.event.RegisterCommandsEvent
+import net.minecraftforge.event.entity.living.LivingDamageEvent
 import net.minecraftforge.event.entity.player.PlayerEvent.Clone
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber
@@ -25,6 +28,19 @@ object EventInit {
                 UdemyMod.ID + "homepos",
                 event.original.persistentData.getIntArray(UdemyMod.ID + "homepos")
             )
+        }
+    }
+
+    @SubscribeEvent
+    fun setEntityOnFireWhenHit(event: LivingDamageEvent) {
+        if(!event.entity.level.isClientSide) {
+            if(event.source.directEntity is Player) {
+                val player = event.source.directEntity as Player
+                if (player.mainHandItem.item == Items.NETHER_BRICK) {
+                    player.mainHandItem.shrink(1)
+                    event.entityLiving.setSecondsOnFire(2)
+                }
+            }
         }
     }
 }
