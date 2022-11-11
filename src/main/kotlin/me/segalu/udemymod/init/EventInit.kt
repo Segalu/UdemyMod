cff22b6a01e813cad6d13478be8585aaca84d75b
Segalu <SebaGLuna@outlook.com>
2022-11-11 13:01:46 -0600

    Lesson: Custom Villager Trades

--- src/main/kotlin/me/segalu/udemymod/init/EventInit.kt
+++ src/main/kotlin/me/segalu/udemymod/init/EventInit.kt
@@ -6,14 +6,18 @@ import me.segalu.udemymod.command.SetHomeCommand
 import me.segalu.udemymod.config.UdemyModClientConfigs
 import me.segalu.udemymod.util.SegaluTitleScreen
 import net.minecraft.client.gui.screens.TitleScreen
+import net.minecraft.world.entity.npc.VillagerProfession
 import net.minecraft.world.entity.player.Player
+import net.minecraft.world.item.ItemStack
 import net.minecraft.world.item.Items
+import net.minecraft.world.item.trading.MerchantOffer
 import net.minecraftforge.api.distmarker.Dist
 import net.minecraftforge.api.distmarker.OnlyIn
 import net.minecraftforge.client.event.ScreenOpenEvent
 import net.minecraftforge.event.RegisterCommandsEvent
 import net.minecraftforge.event.entity.living.LivingDamageEvent
 import net.minecraftforge.event.entity.player.PlayerEvent.Clone
+import net.minecraftforge.event.village.VillagerTradesEvent
 import net.minecraftforge.eventbus.api.SubscribeEvent
 import net.minecraftforge.fml.common.Mod.EventBusSubscriber
 import net.minecraftforge.server.command.ConfigCommand
@@ -57,4 +61,28 @@ object EventInit {
             event.screen = SegaluTitleScreen
         }
     }
+
+    @SubscribeEvent
+    fun addCustomTrades(event: VillagerTradesEvent) {
+        if (event.type == VillagerProfession.FARMER) {
+            val trades = event.trades
+            val stack = ItemStack(ItemInit.TURNIP, 12)
+            val villagerLevel = 1
+
+            trades[villagerLevel].add { trader, rand ->
+                MerchantOffer(ItemStack(Items.EMERALD, 2), stack, 10, 50, 0.02F)
+            }
+        }
+
+        if (event.type == VillagerProfession.TOOLSMITH) {
+            val trades = event.trades
+            val stack = ItemStack(ItemInit.COBALT_PAXEL, 1)
+            val villagerLevel = 3
+
+            trades[villagerLevel].add { trader, rand ->
+                MerchantOffer(ItemStack(Items.EMERALD, 12), stack, 4, 120, 0.09F)
+            }
+        }
+    }
+
 }
--- src/main/kotlin/me/segalu/udemymod/world/gen/ModEntityGeneration.kt
+++ src/main/kotlin/me/segalu/udemymod/world/gen/ModEntityGeneration.kt
@@ -1,6 +1,5 @@
 package me.segalu.udemymod.world.gen
 
-import me.segalu.udemymod.entity.RaccoonEntity
 import me.segalu.udemymod.init.EntityInit
 import net.minecraft.resources.ResourceKey
 import net.minecraft.world.entity.EntityType
@@ -10,22 +9,49 @@ import net.minecraft.world.level.biome.MobSpawnSettings
 import net.minecraftforge.event.world.BiomeLoadingEvent
 import java.util.*
 
-
 object ModEntityGeneration {
     fun onEntitySpawn(event: BiomeLoadingEvent) {
         addEntityToAllOverworldBiomes(event, EntityInit.RACCOON, 40, 2, 4)
-        addEntityToSpecificBiomes(event, EntityInit.TIGER, 40, 2, 4, Biomes.JUNGLE, Biomes.BEACH, Biomes.BAMBOO_JUNGLE, Biomes.SAVANNA, Biomes.SPARSE_JUNGLE, Biomes.SAVANNA_PLATEAU)
+        addEntityToSpecificBiomes(
+            event,
+            EntityInit.TIGER,
+            40,
+            2,
+            4,
+            Biomes.JUNGLE,
+            Biomes.BEACH,
+            Biomes.BAMBOO_JUNGLE,
+            Biomes.SAVANNA,
+            Biomes.SPARSE_JUNGLE,
+            Biomes.SAVANNA_PLATEAU
+        )
     }
 
-    private fun addEntityToAllBiomesExceptThese(event: BiomeLoadingEvent, type: EntityType<*>,weight: Int, minCount: Int, maxCount: Int, vararg biomes: ResourceKey<Biome>) {
-        val isBiomeSelected: Boolean = biomes.map(ResourceKey<*>::location).map(Any::toString).any { it == event.name.toString() }
+    private fun addEntityToAllBiomesExceptThese(
+        event: BiomeLoadingEvent,
+        type: EntityType<*>,
+        weight: Int,
+        minCount: Int,
+        maxCount: Int,
+        vararg biomes: ResourceKey<Biome>
+    ) {
+        val isBiomeSelected: Boolean =
+            biomes.map(ResourceKey<*>::location).map(Any::toString).any { it == event.name.toString() }
         if (!isBiomeSelected) {
             addEntityToAllBiomes(event, type, weight, minCount, maxCount)
         }
     }
 
-    private fun addEntityToSpecificBiomes(event: BiomeLoadingEvent, type: EntityType<*>,weight: Int, minCount: Int, maxCount: Int, vararg biomes: ResourceKey<Biome>) {
-        val isBiomeSelected: Boolean = biomes.map(ResourceKey<*>::location).map(Any::toString).any { it == event.name.toString() }
+    private fun addEntityToSpecificBiomes(
+        event: BiomeLoadingEvent,
+        type: EntityType<*>,
+        weight: Int,
+        minCount: Int,
+        maxCount: Int,
+        vararg biomes: ResourceKey<Biome>
+    ) {
+        val isBiomeSelected: Boolean =
+            biomes.map(ResourceKey<*>::location).map(Any::toString).any { it == event.name.toString() }
         if (isBiomeSelected) {
             addEntityToAllBiomes(event, type, weight, minCount, maxCount)
         }
